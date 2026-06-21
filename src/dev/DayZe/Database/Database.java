54a5d71a5f16ca525a510289e17f977a18193a08
DayZe <247143823+DayZe-Fr@users.noreply.github.com>
2026-06-21 18:48:19 +1000

    added database connection, created sign in UI, made signin controller take input

--- src/dev/DayZe/Database/Database.java
+++ src/dev/DayZe/Database/Database.java
@@ -1,4 +1,22 @@
 package dev.DayZe.Database;
 
+import dev.DayZe.tools;
+
+import java.sql.*;
+
 public class Database {
+    public static Connection connection;
+    private tools _tools;
+
+
+
+    public Database(tools tools) {
+        _tools = tools;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/attendance_tracker", "root", "");
+            _tools.print("connected");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/dev/DayZe/View/GUI.java
+++ src/dev/DayZe/View/GUI.java
@@ -9,8 +9,22 @@ public class GUI extends JFrame {
 
     private CardLayout layout;
 
-    private JPanel signIn;
-    private JLabel _LBL_signIn;
+    ///error key
+    private JLabel errorText;
+    private Color errorColour = new Color(255, 0, 0);
+
+
+    // =================================================
+    // sign in
+    private JPanel _signIn;
+    // username
+    private JLabel _LBL_username;
+    private JTextField _INP_user;
+    //password
+    private JLabel _LBL_password;
+    private JPasswordField _INP_pass;
+    //other
+    public JButton trySubmit;
 
     public GUI(int Height, int Width) {
         var screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -18,7 +32,50 @@ public class GUI extends JFrame {
         _mainPanel = new JPanel();
 
 
+        // ==============================================
+        _signIn = new JPanel();
+        // user
+        _LBL_username = new JLabel("Username");
+        _LBL_username.setAlignmentX(Component.CENTER_ALIGNMENT);
+        _LBL_username.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        _INP_user = new JTextField(30);
 
+        // pass
+        _LBL_password = new JLabel("Password");
+        _LBL_password.setAlignmentX(Component.CENTER_ALIGNMENT);
+        _LBL_password.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        _INP_pass = new JPasswordField(30);
+        _INP_pass.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        //other
+        trySubmit = new JButton("Sign In");
+
+
+        //setting panel
+        _signIn.setLayout(new GridLayout(10, -1));
+        _signIn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //adding user
+        _signIn.add(_LBL_username);
+        _signIn.add(_INP_user);
+        //adding pass
+        _signIn.add(_LBL_password);
+        _signIn.add(_INP_pass);
+        //adding other
+        _signIn.add(trySubmit);
+
+
+
+        // ==============================================
+        //adding components
+
+        _mainPanel.add(_signIn, "SignIn");
+
+
+
+
+
+        this.add(_mainPanel);
 
 
         // setting stuff
@@ -29,7 +86,37 @@ public class GUI extends JFrame {
         this.setVisible(true);
     }
 
+    /// Get the entered User
+    public String getEnteredUser() {
+        return _INP_user.getText();
+    }
 
+    /// Get the entered password
+    public String getEnteredPass() {
+        return _INP_pass.getText();
+    }
+
+    /// Clean the log in fields
+    public void cleanLogin() {
+        _INP_user.setText("");
+        _INP_pass.setText("");
+        _signIn.remove(errorText);
+        errorText = null;
+    }
+
+    /** Displays an Error on GUI
+    @param sentError "Error Text" **/
+    public void displayError(String sentError) {
+        if (errorText == null) {
+            errorText = new JLabel(sentError);
+            errorText.setForeground(errorColour);
+            _signIn.add(errorText);
+            _signIn.revalidate();
+            _signIn.repaint();
+        } else {
+            errorText.setText(sentError);
+        }
+    }
 
 
 
